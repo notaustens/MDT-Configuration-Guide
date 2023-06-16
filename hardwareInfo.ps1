23b4d287eea45199989a1403609cc2522930d673
Austen Summers <105608906+notaustens@users.noreply.github.com>
2023-06-16 09:54:56 -0400

    Update hardwareInfo.ps1

--- hardwareInfo.ps1
+++ hardwareInfo.ps1
@@ -1,4 +1,4 @@
-﻿# Pulls system hardware identification information 
+# Exports hardware identification information to an .html file for asset tracking
 
 wmic computersystem get manufacturer,model /format:hform >>"C:\Users\Public\Desktop\hardwareInfo.html"
 wmic bios get manufacturer,serialnumber /format:hform >>"C:\Users\Public\Desktop\hardwareInfo.html"
