252df2022c5a54ce0b2840c4843b05787e214658
Austen Summers <105608906+notaustens@users.noreply.github.com>
2023-06-16 09:23:06 -0400

    Update hardenRegistry.ps1

--- hardenRegistry.ps1
+++ hardenRegistry.ps1
@@ -1,16 +1,16 @@
-#Requires -RunAsAdministrator
+# Requires -RunAsAdministrator
 
-#OneLine
+# OneLine
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-
-#Or
+    
+# Or
 $Loc = Get-Location
 "Security.Principal.Windows" | % { IEX "( [ $_`Principal ] [$_`Identity ]::GetCurrent() ).IsInRole( 'Administrator' )" } | ? {
     $True | % { $Arguments =  @('-NoProfile','-ExecutionPolicy Bypass','-NoExit','-File',"`"$($MyInvocation.MyCommand.Path)`"","\`"$Loc\`"");
     Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Arguments; } }
 
-#Save Actual Preference
+# Save Actual Preference
 $errpref = $ErrorActionPreference
 $ErrorActionPreference = "silentlycontinue"
 
@@ -24,18 +24,18 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Secur
 New-Item -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319"
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -Name "SchUseStrongCrypto" -Type DWord -Value 1
 
-#<serverExample> should be changed to reflect the hostname of your deployment server
+# <serverExample> should be changed to reflect the hostname of your deployment server
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c reg.exe import `"\\<serverExample>\Resources\Ingress Preparation\OneDrive.reg`"" -Wait 
 
-#Disable OneDrive FilieSync via Group Policies
+# Disable OneDrive FilieSync via Group Policies
 force-mkdir "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive"
 sp "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive" "DisableFileSyncNGSC" 1
 
-#Remove remaining OneDrive files
+# Remove remaining OneDrive files
 rm -Recurse -Force -ErrorAction SilentlyContinue "$env:localappdata\Microsoft\OneDrive"
 rm -Recurse -Force -ErrorAction SilentlyContinue "$env:programdata\Microsoft OneDrive"
 rm -Recurse -Force -ErrorAction SilentlyContinue "C:\OneDriveTemp"
 
-#Restore Previous Preference
+# Restore Previous Preference
 $ErrorActionPreference = $errpref
 
